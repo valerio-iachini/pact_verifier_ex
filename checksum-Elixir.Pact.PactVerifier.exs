e12bb7955344b79a086f3a275542afc6c0abdd07
Valerio <valerio.iachini@prima.it>
2026-01-23 19:59:38 +0100

    update checksum

---
+++ checksum-Elixir.Pact.PactVerifier.exs
@@ -0,0 +1,10 @@
+%{
+  "libpact_verifier_nif-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:c41ab6172632206520b3f3b1bc2a51639f9e479f3f002edea589a155f1f8c7e1",
+  "libpact_verifier_nif-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:c1487f21d17abfa2ff2ee62b9e4d406b135cefa879ecc10ce075cf2392a2b1da",
+  "libpact_verifier_nif-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:51a572c1bdf0f36c9905108077099508de8470c3544a3dabee14f5f4413282af",
+  "libpact_verifier_nif-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:2a441fd1db4ec18a6b05945cd37dba51caf8389bdade8ca9974babf873d8897a",
+  "libpact_verifier_nif-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ba0b3659b811b5351ec559ff53623e88860d7031ff361c531ed6256e79acfbe1",
+  "libpact_verifier_nif-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:88ca7d7d64299b0b3a7a8a89a0210e807367691af1425c5cdc226bf7876d37b8",
+  "pact_verifier_nif-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:d68402eb6bcde6a80ee4211a581951aaa819e7b5a34a26aa1bc49cd8c69f8600",
+  "pact_verifier_nif-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:97cab927f327317ddcdad42853318293ffbb98cd627436515b3f15d070f9aa9e",
+}
