613ad950a4df1c34e2aea4de26446a5faf2bd8a5
Valerio <valerio.iachini@prima.it>
2026-02-02 10:30:33 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PactElixir.MixProject do
   def project do
     [
       app: :pact_verifier_ex,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
